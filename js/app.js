153b97a3c48535fed0535b148ab9253d7c0f7d04
Zual Rana <freelancerzual@gmail.com>
2023-04-20 13:44:25 +0600

    get the player name

--- js/app.js
+++ js/app.js
@@ -4,5 +4,6 @@ console.log(selectBtnS);
 for (const btn of selectBtnS) {
   btn.addEventListener("click", function () {
     btn.setAttribute("disabled", true);
+    const playerName = btn.parentNode.parentNode.children[0].innerText;
   });
 }
