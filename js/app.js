486cee65ad03b963264e5f5209c99cd36a0c4332
Zual Rana <freelancerzual@gmail.com>
2023-04-20 13:47:23 +0600

    get & set the player name in the list

--- js/app.js
+++ js/app.js
@@ -5,5 +5,10 @@ for (const btn of selectBtnS) {
   btn.addEventListener("click", function () {
     btn.setAttribute("disabled", true);
     const playerName = btn.parentNode.parentNode.children[0].innerText;
+
+    const ul = document.getElementById("list-container");
+    const li = document.createElement("li");
+    li.innerText = playerName;
+    ul.appendChild(li);
   });
 }
