5fbd54d0edc2b4afe054fd699654ca894cbe1e5c
Zual Rana <freelancerzual@gmail.com>
2023-04-20 14:02:08 +0600

    get & set per player value

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 const selectBtnS = document.getElementsByClassName("select-btn");
 console.log(selectBtnS);
-
+let count = 1;
 for (const btn of selectBtnS) {
   btn.addEventListener("click", function () {
-    btn.setAttribute("disabled", true);
     const playerName = btn.parentNode.parentNode.children[0].innerText;
 
     const ul = document.getElementById("list-container");
     const li = document.createElement("li");
     li.innerText = playerName;
     ul.appendChild(li);
+    btn.setAttribute("disabled", true);
   });
 }
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const perPlayerField = document.getElementById("inputPerPlyaer");
+  const perPlayerValue = parseFloat(perPlayerField.value);
+  const displayExpenses = document.getElementById("player-expenses");
+  displayExpenses.innerText = perPlayerValue * 5;
+});
